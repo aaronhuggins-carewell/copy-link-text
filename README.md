58e447642ab26639b7962a0cef79cfc9d13966a3
Marco Kellershoff <hello@gorilla.moe>
2018-09-18 12:32:04 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+Copy Link Text
+==============
+
+Lets you copy a link's text to the clipboard via the context menu.
+
+**Note: - After installation, you need to refresh open tabs**
+
---
+++ src/boot.js
@@ -0,0 +1,18 @@
+(function(window, document) {
+        'use strict';
+
+        /* global chrome */
+
+        const _browser = chrome;
+        let target;
+
+        document.addEventListener('contextmenu', (e) => {
+                target = e.target;
+        }, false);
+
+        _browser.runtime.onMessage.addListener(() => {
+                _browser.runtime.sendMessage(null, target.innerText.trim());
+        });
+
+})(window, document);
+
