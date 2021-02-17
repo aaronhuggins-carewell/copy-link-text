ea2c6a4aff22a0de025ef7b3c3cef6292918536a
Marco Kellershoff <marco@kellershoff.net>
2021-02-17 17:44:49 +0100

    Remove unused permissions

--- src/background.js
+++ src/background.js
@@ -1,31 +1,29 @@
-(function(){
-        'use strict';
+(function () {
+  'use strict'
 
-        /* global chrome */
+  /* global chrome */
 
-        const _browser = chrome;
-        const _menus = _browser.contextMenus;
-        const dummyTextArea = document.createElement('textarea');
-        document.body.appendChild(dummyTextArea);
+  const _browser = chrome
+  const _menus = _browser.contextMenus
+  const dummyTextArea = document.createElement('textarea')
+  document.body.appendChild(dummyTextArea)
 
-        _menus.create({
-                id: 'copy-link-text',
-                title: _browser.i18n.getMessage('menuItemCopyLinkText'),
-                contexts: ['all']
-        });
+  _menus.create({
+    id: 'copy-link-text',
+    title: _browser.i18n.getMessage('menuItemCopyLinkText'),
+    contexts: ['all']
+  })
 
-        _menus.onClicked.addListener((info, tab) => {
-                // Send an empty message to the content script, which will then send back the
-                // link text for us to copy on the clipboard.
-                _browser.tabs.sendMessage(tab.id, "", {frameId: info.frameId});
-        });
+  _menus.onClicked.addListener((info, tab) => {
+    // Send an empty message to the content script, which will then send back the
+    // link text for us to copy on the clipboard.
+    _browser.tabs.sendMessage(tab.id, '', { frameId: info.frameId })
+  })
 
-        _browser.runtime.onMessage.addListener((linkText) => {
-                dummyTextArea.value = linkText;
-                dummyTextArea.select();
-                document.execCommand('copy');
-                dummyTextArea.value = '';
-        });
-
-})();
-
+  _browser.runtime.onMessage.addListener((linkText) => {
+    dummyTextArea.value = linkText
+    dummyTextArea.select()
+    document.execCommand('copy')
+    dummyTextArea.value = ''
+  })
+})()
--- src/boot.js
+++ src/boot.js
@@ -1,18 +1,16 @@
-(function(window, document) {
-        'use strict';
+(function (window, document) {
+  'use strict'
 
-        /* global chrome */
+  /* global chrome */
 
-        const _browser = chrome;
-        let target;
+  const _browser = chrome
+  let target
 
-        document.addEventListener('contextmenu', (e) => {
-                target = e.target;
-        }, false);
+  document.addEventListener('contextmenu', (e) => {
+    target = e.target
+  }, false)
 
-        _browser.runtime.onMessage.addListener(() => {
-                _browser.runtime.sendMessage(null, target.innerText.trim());
-        });
-
-})(window, document);
-
+  _browser.runtime.onMessage.addListener(() => {
+    _browser.runtime.sendMessage(null, target.innerText.trim())
+  })
+})(window, document)
